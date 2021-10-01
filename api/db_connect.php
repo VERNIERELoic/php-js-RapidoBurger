8b9b26a9e6875bfc5014441da820d9dfbfddfa86
l.verniere <loic.verniere@cpe.fr>
2021-10-01 16:36:48 +0200

    add-class-user

--- api/db_connect.php
+++ api/db_connect.php
@@ -1,17 +1,14 @@
 <?php
-$host = 'localhost';
-$port = "3306";
-$dbname = 'rapido';
-$dbuser = 'loic';
-$dbpass = 'toor';
+
+$dbhost = "127.0.0.1";
+$dbname = "rapido";
+$dbuser = "loic";
+$dbpass = "toor";
 
 try {
-  //On test la connexion à la base de donnée
-  $bdd = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  reponse_json($success, $data, 'Connexion à la base de donnée reussi');
+  // On se connecte à MySQL
+  $bdd = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
 } catch (Exception $e) {
-  //Si la connexion n'est pas établie, on stop le chargement de la page.
-  reponse_json($success, $data, 'Echec de la connexion à la base de données');
-  exit();
+  // En cas d'erreur, on affiche un message et on arrête tout
+  die('Erreur : ' . $e->getMessage());
 }
--- api/user.php
+++ api/user.php
@@ -1,20 +1,77 @@
 <?php
-include('template.php');
+
+include("db_connect.php");
+
+class User
+{
+    private $id;
+    private $name;
+    private $username;
+    private $birthday;
+    private $password;
+    private $city;
+    private $country;
+    private $zip_code;
+    private $email;
+    private $bdd;
 
 
+    public function getUsers()
+    {
+        $response =  $this->bdd->query('SELECT * FROM user');
+        $data = $response->fetchAll();
+        return $data;
+    }
 
+    public function getUser($id)
+    {
+        try {
+            $response =  $this->bdd->query('SELECT * FROM user WHERE id = ' + $id);
+            $data = $response->fetchAll();
+            return $data;
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
 
-$requete = $pdo->prepare("SELECT * FROM `user`");
-var_dump("EO LA".$requete->execute());
+    public function insertUser($user)
+    {
+        try {
+            $sql = "INSERT INTO user (id, name, username, email, birthday, country, city, zip_code)
+                VALUES ($user->id, $user->name, $user->username, $user->birthday, $user->password, $user->city, 
+                $user->country, $user->zip_code, $user->email)";
+            $this->bdd->exec($sql);
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
 
-if ($requete->execute()) {
-    $resultats = $requete->fetchAll();
+    public function dropUser($user)
+    {
+        try {
+            $sql = "DELETE FROM user WHERE id=$this.id";
+            $this->bdd->exec($sql);
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
 
-    $success = true;
-    $data['id'] = count($resultats);
-    $data['nom'] = $resultats;
-} else {
-    $msg = "Une erreur s'est produite";
+    public function update($user)
+    {
+        try {
+            $sql = "UPDATE user
+                    SET id = $user->id, 
+                        name = $user->name, 
+                        username = $user->username, 
+                        email = $user->email, 
+                        birthday = $user->birthday, 
+                        countrie = $user->country,
+                        city = $user->city, 
+                        zip_code = $user->zip_code
+                    WHERE id = $this->id";
+            $this->bdd->exec($sql);
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+    }
 }
-
-var_dump($resultats);
